191e24f9cec9a7e0552ae82449efc4e2f6675230
Artem <ermilintemka@gmail.com>
2022-06-28 19:39:27 +0300

    add HW1

---
+++ HW1/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное 
+число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+
+
+Console.WriteLine("Введите трехзначное число");
+string number = Console.ReadLine();
+int numberN = int.Parse(number);
+
+Console.WriteLine($"введенное число {numberN}");
+
+int secondN = (numberN / 10) % 10;
+
+Console.WriteLine("Полученное число {0}", secondN);
